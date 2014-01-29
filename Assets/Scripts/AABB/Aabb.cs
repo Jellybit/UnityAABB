f5bde3c96910ce09cff67e4e3c19110b5623fb55
Jellybit <segaspam@gmail.com>
2014-01-29 08:30:32 -0500

    Added some new functions. Cleaned up a little.

--- Assets/Scripts/AABB/Aabb.cs
+++ Assets/Scripts/AABB/Aabb.cs
@@ -10,7 +10,7 @@ public static class Aabb
 	public static Dictionary<GameObject, Rect> trackedColliders = new Dictionary<GameObject, Rect>();
 	
 	// This checks the intersection between two rectangles. It is used by all the other collision methods.
-	public static bool Intersect( Rect a, Rect b ) 
+	public static bool Intersecting( Rect a, Rect b ) 
 	{
 		// Basic AABB collision detection. All of these must be true for there to be a collision.
 		bool comp1 = a.yMin > b.yMax;
@@ -23,7 +23,7 @@ public static class Aabb
 	}
 
 	// Use this one on your game object like so: gameObject.Colliding( otherGameObject );
-	public static bool CollidingWith( this GameObject self, GameObject other )
+	public static bool isCollidingWith( this GameObject self, GameObject other )
 	{
 
 
@@ -34,37 +34,37 @@ public static class Aabb
 		Rect bBox = other.BoxToRect();
 
 		// Find out if these guys intersect
-		return Intersect ( aBox, bBox );
+		return Intersecting ( aBox, bBox );
 	}
 
 	// Same thing, different approach
 	public static bool Colliding( GameObject a, GameObject b )
 	{
-		return a.CollidingWith( b );
+		return a.isCollidingWith( b );
 	}
 
 
-	public static bool CollidingWithTrackedColliders( this GameObject self )
+	public static bool isCollidingWithTrackedColliders( this GameObject self )
 	{
 
 		self.UpdateMyCollisions();
-		return self.RectCollidingWithObjectsInDictionary( trackedColliders[self], trackedColliders );
+		return self.RectCollidingWithDictionaryObjects( trackedColliders[self], trackedColliders );
 
 	}
 
-	public static bool CollidingWithObjectsInDictionary( this GameObject self, Dictionary<GameObject, Rect> colliderDictionary )
+	public static bool isCollidingWithDictionaryObjects( this GameObject self, Dictionary<GameObject, Rect> colliderDictionary )
 	{
-		return self.RectCollidingWithObjectsInDictionary( self.BoxToRect(), colliderDictionary );
+		return self.RectCollidingWithDictionaryObjects( self.BoxToRect(), colliderDictionary );
 	}
 
 	public static bool RectCollidingWithTrackedColliders( this GameObject self, Rect testCollider )
 	{
 
-		return self.RectCollidingWithObjectsInDictionary( testCollider, trackedColliders );
+		return self.RectCollidingWithDictionaryObjects( testCollider, trackedColliders );
 		
 	}
 
-	public static bool RectCollidingWithObjectsInDictionary( this GameObject self, Rect testCollider, Dictionary<GameObject, Rect> colliderDictionary )
+	public static bool RectCollidingWithDictionaryObjects( this GameObject self, Rect testCollider, Dictionary<GameObject, Rect> colliderDictionary )
 	{
 		
 		bool colliding = false;
@@ -75,11 +75,11 @@ public static class Aabb
 		{
 			if ( self != pair.Key )
 			{
-				if (Intersect( testCollider, pair.Value ))
+				if (Intersecting( testCollider, pair.Value ))
 				{
 					colliding = true;
 					
-					SendCollisionInfo( self, pair.Key );
+					AnnounceCollision( self, pair.Key );
 				}
 				
 				
@@ -91,27 +91,33 @@ public static class Aabb
 	}
 
 	// This converts a BoxCollider2D to a rectangle for use in determining an intersection
-	public static Rect BoxToRect ( this GameObject a )
+	public static Rect BoxToRectAtPosition ( this GameObject a, Vector3 testPositionV3 )
 	{
 		// Finding the BoxCollider2D for the GameObject.
-		BoxCollider2D aCollider = a.GetComponent<BoxCollider2D> ();
-		
-		// Grabbing the GameObject position, converting it to Vector2.
-		Vector2 aPos = a.transform.position.V2();
-		
-		// Now that we have the object's worldspace location, we offset that to the BoxCollider's center
-		aPos += aCollider.center;
+		BoxCollider2D boxCollider = a.GetComponent<BoxCollider2D> ();
+
+		// Converting to vector 2 so we can add another vector 2 to it.
+		Vector2 testPosition = testPositionV3.V2();
+
+		// Using a given worldspace location, we offset that to the BoxCollider's center
+		testPosition += boxCollider.center;
 		// From the center, we find the top/left corner by cutting the total height/width in half and 
 		// offset by that much
-		float sizeX = aCollider.size.x * a.transform.localScale.x;
-		float sizeY = aCollider.size.y * a.transform.localScale.y;
-
-		aPos.x -= Mathf.Abs( sizeX )/2;
-		aPos.y += Mathf.Abs( sizeY )/2;
+		float rectWidth = boxCollider.size.x * a.transform.localScale.x;
+		float rectHeight = boxCollider.size.y * a.transform.localScale.y;
+		
+		testPosition.x -= Mathf.Abs( rectWidth )/2;
+		testPosition.y += Mathf.Abs( rectHeight )/2;
 		
 		// Create a rectangle based on the top/left corner, and extending the rectangle 
 		// to the width/height
-		return new Rect ( aPos.x, aPos.y, sizeX, -sizeY );
+		return new Rect ( testPosition.x, testPosition.y, rectWidth, -rectHeight );
+	}
+
+	// This converts a BoxCollider2D to a rectangle using the object's actual position
+	public static Rect BoxToRect ( this GameObject self )
+	{
+		return self.BoxToRectAtPosition( self.transform.position );
 	}
 	
 	// Converts a Vector 3 to Vector 2
@@ -135,7 +141,7 @@ public static class Aabb
 	}
 
 	// Sends collision information to both objects involved in the collision
-	private static void SendCollisionInfo ( GameObject object1, GameObject object2 ) 
+	private static void AnnounceCollision ( GameObject object1, GameObject object2 ) 
 	{
 		
 		object1.NotifyOfCollisionWith( object2 );
@@ -175,5 +181,26 @@ public static class Aabb
 	{
 		trackedColliders[self] = testRectangle;
 	}
+
+	public static Dictionary<GameObject, Rect> UpdateAllCollisionsInThisDictionary( this Dictionary<GameObject, Rect> colliderDictionary )
+	{
+		
+		Dictionary<GameObject, Rect> colliderDictionaryCopy = new Dictionary<GameObject, Rect>(colliderDictionary);
+		
+		foreach (var pair in colliderDictionaryCopy)
+		{
+			colliderDictionaryCopy[pair.Key] = pair.Key.BoxToRect();			
+		}
+		
+		return colliderDictionaryCopy;
+	}
+
+	public static void UpdateAllTrackedCollisions( )
+	{
+		trackedColliders = trackedColliders.UpdateAllCollisionsInThisDictionary();
+	}
 	
 }
+
+// check point for collision
+// check for collision without announcing
